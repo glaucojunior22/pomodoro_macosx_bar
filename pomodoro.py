70eda7cb393a391af1faf949f14bde26d8da40d0
Glauco Junior <glaucojunior22@gmail.com>
2020-03-02 15:06:01 -0300

    Change the start button to a set timer for a dinamic timer.

--- pomodoro.py
+++ pomodoro.py
@@ -1,25 +1,28 @@
 import rumps
 
-rumps.debug_mode(True)
+#rumps.debug_mode(True)
 
 
 class PomodoroApp(object):
     def __init__(self):
         self.config = {
             'app_name': 'Pomodoro',
+            'set_timer': 'Set Timer minutes',
             'start': 'Start Timer',
             'pause': 'Pause Timer',
             'continue': 'Continue Timer',
             'stop': 'Stop Timer',
             'break_message': 'Time is up! Take a break ;)',
-            'interval': 1500
+            'interval': 25 * 60
         }
         self.app = rumps.App(self.config['app_name'])
         self.timer = rumps.Timer(self.on_tick, 1)
         self.interval  = self.config['interval']
         self.set_up_menu()
-        self.start_pause_button = rumps.MenuItem(title=self.config['start'],
-                                                 callback=self.start_timer)
+        self.start_pause_button = rumps.MenuItem(
+            title=self.config['set_timer'],
+            callback=self.set_timer_minutes
+        )
         self.stop_button = rumps.MenuItem(title=self.config['stop'],
                                           callback=None)
         self.app.menu = [self.start_pause_button, self.stop_button]
@@ -27,7 +30,32 @@ class PomodoroApp(object):
     def set_up_menu(self):
         self.timer.stop()
         self.timer.count = 0
-        self.app.title = '🍅'
+        self.app.icon = 'assets/icon.png'
+        self.app.title = ''
+
+    def set_timer_minutes(self, sender):
+        window = rumps.Window(
+            message='Enter the number of minutes for the timer',
+            title='Timer Configuration',
+            ok='Start Timer',
+            cancel='Cancel',
+            default_text='25',
+            dimensions=(50, 20)
+        )
+        response = window.run()
+        if response.clicked:
+            try:
+                minutes = int(response.text)
+                self.interval = minutes * 60
+                self.start_timer()
+            except ValueError:
+                rumps.notification(
+                    title='Value Error',
+                    subtitle='',
+                    message='The value: {} is not a valid number.'.format(
+                        response.text
+                    )
+                )
 
     def on_tick(self, sender):
         time_left = sender.end - sender.count
@@ -44,21 +72,26 @@ class PomodoroApp(object):
             self.app.title = '{:2d}:{:02d}'.format(mins, secs)
         sender.count += 1
 
-    def start_timer(self, sender):
-        if sender.title.lower().startswith(('start', 'continue')):
-            if sender.title == self.config['start']:
-                self.timer.count = 0
-                self.timer.end = self.interval
-            sender.title = self.config['pause']
-            self.timer.start()
-        else:
+    def start_timer(self):
+        self.start_pause_button.title = self.config['pause']
+        self.start_pause_button.set_callback(self.pause_continue_timer)
+        self.timer.count = 0
+        self.timer.end = self.interval
+        self.timer.start()
+
+    def pause_continue_timer(self, sender):
+        if sender.title == self.config['pause']:
             sender.title = self.config['continue']
             self.timer.stop()
+        else:
+            sender.title = self.config['pause']
+            self.timer.start()
 
     def stop_timer(self, sender=None):
         self.set_up_menu()
         self.stop_button.set_callback(None)
-        self.start_pause_button.title = self.config['start']
+        self.start_pause_button.title = self.config['set_timer']
+        self.start_pause_button.set_callback(self.set_timer_minutes)
 
     def run(self):
         self.app.run()
